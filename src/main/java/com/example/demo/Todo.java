134c8f6dc0c95f79a95fee168f78eb5446721003
BrandonGS <brandon@reservamos.com>
2024-08-28 00:04:12 -0600

    ft: update TODO as Entity for JPA

--- src/main/java/com/example/demo/Todo.java
+++ src/main/java/com/example/demo/Todo.java
@@ -1,3 +1,65 @@
 package com.example.demo;
 
-public record Todo(long id, String description, boolean isDone) {} 
+import java.util.Objects;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.Table;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.Id;
+
+
+@Entity
+@Table(name = "todos")
+class Todo {
+    private @Id 
+    @GeneratedValue Long id;
+    private String description;
+    private boolean isDone;
+
+    Todo() {}
+
+    Todo(String description, boolean isDone) {
+        this.description = description;
+        this.isDone = isDone;
+    }
+
+    public Long getId() {
+        return this.id;
+    }
+
+    public String getDescription() {
+        return this.description;
+    }
+
+    public boolean getIsDone() {
+        return this.isDone;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public void setIsDone(boolean isDone) {
+        this.isDone = isDone;
+    }
+    
+   @Override       
+   public boolean equals(Object o) {
+    if (this == o) return true;
+    if (!(o instanceof Todo)) return false;
+    Todo todo = (Todo) o;
+    return Objects.equals(this.id, todo.id) && Objects.equals(this.description, todo.description)
+        && this.isDone == todo.isDone;
+   }
+
+   @Override
+   public int hashCode() {
+    return Objects.hash(this.id, this.description, this.isDone);
+   }
+
+   @Override
+   public String toString() {
+    return "Todo{" + "id=" + this.id + ", description='" + this.description + '\'' + ", isDone=" + this.isDone + '}';
+   }
+    
+}
